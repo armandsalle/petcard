2186ac747c24462a6b732a2fb6b0b39623317af4
armand <armand.salle@gmail.com>
2024-04-27 23:44:49 +0200

    refactor: remove dead code

--- src/envs.ts
+++ src/envs.ts
@@ -24,11 +24,3 @@ export const env = createEnv({
     DRIZZLE_DATABASE_URL: process.env.DRIZZLE_DATABASE_URL,
   },
 })
-
-// PUBLIC_ASTRO_APP_CLERK_PUBLISHABLE_KEY="pk_test_Y2hpZWYtZWdyZXQtNjUuY2xlcmsuYWNjb3VudHMuZGV2JA"
-// CLERK_SECRET_KEY="sk_test_8lEuyUT4VBxnj2FIM494Sr5ew2vifL1lkymqrA4sez"
-
-// PUBLIC_ASTRO_APP_CLERK_SIGN_IN_URL="/" # update this if sign in page exists on another path
-// PUBLIC_ASTRO_APP_CLERK_SIGN_UP_URL="/" # update this if sign up page exists on another path
-
-// DRIZZLE_DATABASE_URL="postgresql://petcarddb_owner:LjbpZBM93iPa@ep-late-lab-a281kbc8.eu-central-1.aws.neon.tech/petcarddb?sslmode=require"
